ad86111950b83db83a0931a47ca9e9b7bc1dce49
Starshine System <us@starshines.xyz>
2021-01-01 17:39:23 +0100

    add basic logging

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const axios = require('axios');
 const showdown = require('showdown');
 const sanitizeHtml = require('sanitize-html');
+const bunyan = require('bunyan');
 
 const termPage = fs.readFileSync(path.join(__dirname + '/frontend/term.html'), 'utf8');
 const notFoundPage = fs.readFileSync(path.join(__dirname + '/frontend/404.html'), 'utf8');
@@ -13,6 +14,21 @@ const axinst = axios.create({
     validateStatus: (s) => s < 500,
     baseURL: 'https://berry.starshines.xyz/api/v1'
 });
+
+const log = bunyan.createLogger({
+    name: 'berry-site',
+    streams: [
+        {
+            level: 'info',
+            stream: process.stdout            // log INFO and above to stdout
+        },
+        {
+            level: 'error',
+            path: 'error.log'  // log ERROR and above to a file
+        }
+    ]
+});
+
 const app = express();
 const converter = new showdown.Converter({
     strikethrough: true,
@@ -26,15 +42,16 @@ app.use(express.urlencoded({ extended: true }));
 
 async function getRoot(_, res) {
     let list = (await axinst('/list')).data;
+    log.debug({ msg: "got terms", count: list.len });
     list = list.map(t => {
         return `<li><a href="/term/${encodeURI(t.name.toLowerCase())}">${t.name}</a> (${t.aliases.join(', ') || 'no aliases'})</li>`
     }).join("\n")
     res.send(indexPage.replace('$LIST', list));
+    log.info({ path: "/" }, "served list of terms");
 }
 
 async function getTerm(req, res) {
     let termP = termPage;
-
     let terms = (await axinst('/list')).data;
     let termName = decodeURI(req.params.term.toLowerCase());
     let term;
@@ -57,9 +74,10 @@ async function getTerm(req, res) {
         termP = termP.replace('$CATEGORY_ID', term.category_id);
         termP = termP.replace('$CREATED', new Date(term.created).toDateString());
         res.send(termP);
+        log.info({ path: "/term/", term: termName, found: term.name }, "term found");
     } catch (e) {
-        console.log(e);
         res.send(notFoundPage.replace('$TERM_NAME', sanitizeHtml(termName)));
+        log.info({ path: "/term/", term: termName, found: false }, "term not found");
     }
 }
 
@@ -69,3 +87,4 @@ app.use(express.static(path.join(__dirname, '/frontend/')));
 
 const port = process.env.PORT || 8080;
 app.listen(port);
+log.info(`Server running on port ${port}`);
